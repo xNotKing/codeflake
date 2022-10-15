ee00d7433c0b3c8ac4da3f28729d6febe57cfcd1
xNotKing <87067415+xNotKing@users.noreply.github.com>
2022-10-15 16:59:04 +0200

    9349 Try to fix a vercel bug D:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
-// Trying to fix adapter >:(
 export default defineConfig({
     output: 'server',
     adapter: vercel(),
-  });
+});
